cfff164a5ce7f3f69bb2e04682048650e4b79d9b
Colaborador Ricardo <rar.az400@hotmail.com>
2022-10-02 17:36:47 -0300

    atualização feita pelo colaborador 1

--- src/app/MOCK/mock-dados.ts
+++ src/app/MOCK/mock-dados.ts
@@ -3,5 +3,6 @@ export const mockDados = {
         {id:1, conta: "Itaú", descricao:"Conta Itaú Unibanco"},
         {id:2, conta: "Santander", descricao:"Conta Santander"},
         {id:3, conta: "Nubank", descricao:"Conta Itaú Nubank"},
+        {id:5, conta: "C6 Bank", descricao:"Conta C6 Bbank"},
         ]} 
 }
