2c5e0668f1a49289a4322dc0bd1d8a123ef4233c
Henrique <henry2camargo@gmail.com>
2022-10-02 22:17:36 -0300

    criação do formulario Cartoes e da lista de cartoes add

---
+++ src/app/MOCK/mock-cartoes.ts
@@ -0,0 +1,8 @@
+export const mockCartoes = {
+  getCartoes() {return [
+    {id:1, conta: "PicPay", descricao:"Cartão Debito"},
+    {id:2, conta: "NuBank", descricao:"Cartão Credito"},
+    {id:3, conta: "C6Bank", descricao:"Cartão Debito"},
+    {id:2, conta: "Bradesco", descricao:"Cartão Credito"},
+  ] }
+}
--- src/app/MOCK/mock-dados.ts
+++ src/app/MOCK/mock-dados.ts
@@ -3,5 +3,6 @@ export const mockDados = {
         {id:1, conta: "Itaú", descricao:"Conta Itaú Unibanco"},
         {id:2, conta: "Santander", descricao:"Conta Santander"},
         {id:3, conta: "Nubank", descricao:"Conta Itaú Nubank"},
-        ]} 
+        {id:4, conta: "C6Bank", descricao: "Conta C6Bank"},
+        ]}
 }
