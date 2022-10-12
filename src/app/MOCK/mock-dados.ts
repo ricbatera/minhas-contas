dd633cb1cddba6d177183f8f288869ef0e62b123
Ricardo Alves Roberto <contato@rdrtech.com.br>
2022-10-12 17:11:24 -0300

    lista de saídas estilizada

--- src/app/MOCK/mock-dados.ts
+++ src/app/MOCK/mock-dados.ts
@@ -18,22 +18,23 @@ export const mockDados = {
   },
   getListaSaidas() {
     return [
-      { id: 1, descricao: "Conta  de Luz", obs: "Conta de luz anual.", parcela: { id: 1, dataVencimento: "2022-10-28", dataPagamento: null, status: "Aberto", meioPagto: "Boleto / Débito", valor: 321.15, valorPago: null } },
-      { id: 2, descricao: "Internet", obs: "Live TIM internet e telefone.", parcela: { id: 10, dataVencimento: "2022-10-28", dataPagamento: null, status: "Aberto", meioPagto: "Boleto / Débito", valor: 199.3, valorPago: null } },
+      { id: 1, descricao: "Conta  de Luz", obs: "Conta de luz anual. Mussum Ipsum, cacilds vidis litro abertis. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!Casamentiss faiz malandris se pirulitá.Viva Forevis aptent taciti sociosqu ad litora torquent.Cevadis im ampola pa arma uma pindureta.", parcela: { id: 1, dataVencimento: "2022-10-28", dataPagamento: null, status: "Aberto", meioPagto: "Boleto / Débito", valor: 1321.15, valorPago: null, parcela:"2 de 3" } },
+      { id: 2, descricao: "Internet", obs: "Live TIM internet e telefone.", parcela: { id: 10, dataVencimento: "2022-10-28", dataPagamento: null, status: "Atrasado", meioPagto: "Boleto / Débito", valor: 199.3, valorPago: null, parcela:"2 de 3" } },
+      { id: 3, descricao: "Celular", obs: "Contade celular claro.", parcela: { id: 11, dataVencimento: "2022-10-28", dataPagamento: "2022-10-27", status: "Pago", meioPagto: "Boleto / Débito", valor: 199.3, valorPago: 200, parcela:"2 de 3" } },
     ]
   },
     getListaEntrada() {
       return [
-        { id: 1, descricao: "Faculade", observacao: "faculdade mês de Outubro", valor: "99,99", prvisaoPagto: "2022-10-28", parcela: "1 de 12" },
-        { id: 2, descricao: "Casas Baiha", observacao: "faculdade mês de Novembro", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "2 de 12" },
-        { id: 3, descricao: "Boleto Bradesco", observacao: "faculdade mês de Novembro", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "3 de 12" },
-        { id: 4, descricao: "Boleto Lojas Cem", observacao: "faculdade mês de Novembro", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "4 de 12" },
-        { id: 5, descricao: "Conta Mensal", observacao: "Conta de Internet", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "5 de 12" },
-        { id: 6, descricao: "Conta Mensal", observacao: "Conta de Agúas", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "6 de 12" },
-        { id: 7, descricao: "Conta Mensal", observacao: "Conta de Luz", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "7 de 12" },
-        { id: 8, descricao: "Mercado Livre", observacao: "Compra de uma Jaqueta", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "8 de 12" },
-        { id: 9, descricao: "Mercado Livre", observacao: "Compra de um Fone de Ouvido", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "9 de 12" },
-        { id: 10, descricao: "Mercado Livre", observacao: "Compra de um Teclado", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "10 de 12" },
+        { id: 1, descricao: "Salário", observacao: "salario da Alma Viva", valor: "1121,35,99", prvisaoPagto: "2022-10-28", parcela: "1 de 12" },
+        { id: 2, descricao: "Concerto Note Bia", observacao: "troca de memória do note da bianca", valor: "399,99", prvisaoPagto: "2022-11-28", parcela: "2 de 12" },
+        { id: 3, descricao: "Ponto Nivelo", observacao: "Pontos referente ao mês de setembro", valor: "899,80", prvisaoPagto: "2022-11-28", parcela: "3 de 12" },
+        { id: 4, descricao: "Bob", observacao: "dineiro que emprestei pro BOB", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "4 de 12" },
+        { id: 5, descricao: "Geladeira Mãe", observacao: "Parcela da geladeira que tirei pra mãe", valor: "239", prvisaoPagto: "2022-11-28", parcela: "5 de 12" },
+        // { id: 6, descricao: "Conta Mensal", observacao: "Conta de Agúas", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "6 de 12" },
+        // { id: 7, descricao: "Conta Mensal", observacao: "Conta de Luz", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "7 de 12" },
+        // { id: 8, descricao: "Mercado Livre", observacao: "Compra de uma Jaqueta", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "8 de 12" },
+        // { id: 9, descricao: "Mercado Livre", observacao: "Compra de um Fone de Ouvido", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "9 de 12" },
+        // { id: 10, descricao: "Mercado Livre", observacao: "Compra de um Teclado", valor: "99,99", prvisaoPagto: "2022-11-28", parcela: "10 de 12" },
       ]
     }
   }
--- src/app/sistema/sistema/saidas/lista-saidas/lista-saidas.component.ts
+++ src/app/sistema/sistema/saidas/lista-saidas/lista-saidas.component.ts
@@ -1,15 +1,42 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { mockDados } from 'src/app/MOCK/mock-dados';
 
 @Component({
   selector: 'app-lista-saidas',
   templateUrl: './lista-saidas.component.html',
-  styleUrls: ['./lista-saidas.component.css']
+  styleUrls: ['./lista-saidas.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ListaSaidasComponent implements OnInit {
+  lista = mockDados.getListaSaidas();
+  colunasTabela = ['descricao', 'status', 'Data Pagamento'];
+  expandedElement!: dados | null;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+export interface dados{
+  id: number,
+  descricao: string,
+  obs: string,
+  parcela:{
+    status:string,
+    dataVencimento:Date,
+    dataPagamento: Date,
+    meioPagto: string,
+    valor:number,
+    valorPago: number,
+    parcela: string
+  }
 }
