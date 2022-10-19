19521f6fc9524b4715352a76642ad974249a4695
Henrique <henry2camargo@gmail.com>
2022-10-18 22:47:45 -0300

    tabela entradas

--- src/app/sistema/sistema/entradas/entrada-detalhe/entrada-detalhe.component.ts
+++ src/app/sistema/sistema/entradas/entrada-detalhe/entrada-detalhe.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
 import { mockDados } from 'src/app/MOCK/mock-dados';
+import { EntradaDetalhes } from 'src/app/model/entrada-detalhes';
+import { parcela } from 'src/app/model/model';
+import { IAppState } from 'src/app/store/app.reducer';
 
 @Component({
   selector: 'app-entrada-detalhe',
@@ -10,18 +17,29 @@ import { mockDados } from 'src/app/MOCK/mock-dados';
 
 
 export class EntradaDetalheComponent implements OnInit {
+ idEntrada$ =  this.store.select('app').pipe(map(dado => dado.idEntrada));
  detalhe = mockDados.getEntradaDetalhes
 
+
  form: FormGroup;
+ listaCartoes = mockDados.getCartoes();
+ entradaDetalhe: EntradaDetalhes = mockDados.getEntradaDetalhes();
+ colunasTabela = ['Valor', 'Vencimento', 'Ação']
+ @ViewChild(MatSort) sort!: MatSort;
+
 
  constructor(
+  private store:Store<{app: IAppState}>,
   private fb: FormBuilder
 ) {
   this.form = this.fb.group({
     id: [null],
-    descricao: [null, Validators.required],
-    observacao: [null, Validators.required],
-
+    nome: [null, Validators.required],
+    observacao: [null, Validators.required], 
+    descricao: [null, Validators.required], 
+    dataRecebimento: [null, Validators.required],
+    qtdeParcelas: [null, Validators.required],
+    valoReceber: [null, Validators.required]
   })
 }
 
